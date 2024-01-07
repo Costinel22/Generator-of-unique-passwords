12ba13d5a0fbbe5543c7ba2109af702e92086213
Costinel22 <malaiasi_costinel@yahoo.com>
2024-01-07 19:59:07 +0000

    create the prompt whit the option and alert

---
+++ script.js
@@ -0,0 +1,130 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+    '@',
+    '%',
+    '+',
+    '\\',
+    '/',
+    "'",
+    '!',
+    '#',
+    '$',
+    '^',
+    '?',
+    ':',
+    ',',
+    ')',
+    '(',
+    '}',
+    '{',
+    ']',
+    '[',
+    '~',
+    '-',
+    '_',
+    '.'
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z'
+];
+
+// Function to prompt user for password options
+function getPasswordOptions() {
+    var length = parseInt(prompt("Tel me the caracter number you want to have in password no les 8 or more 128"));
+    if (length < 8 || length > 128) {
+        alert("Please enter your caracter number between 8 and 128 characters");
+        return
+    }
+    specialCharacters = confirm("Du you want to use special characters");
+    numericCharacters = confirm("Do you want to use numeric characters");
+    lowerCasedCharacters = confirm("Do you want to use lowercase characters");
+    upperCasedCharacters = confirm("Do you want to use upercase caracters");
+    return { length, specialCharacters: specialCharacters, numericCharacters: numericCharacters, lowerCasedCharacters: lowerCasedCharacters, upperCasedCharacters: upperCasedCharacters }
+
+}
+
+// Function for getting a random element from an array
+function getRandom(arr) {
+
+}
+
+// Function to generate password with user input
+function generatePassword() {
+    getPasswordOptions();
+
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+
+    var password = generatePassword();
+    var passwordText = document.querySelector('#password');
+
+    passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
